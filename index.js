c063b34810be07384beed1b1e5835d4220f6e8d4
Carole Tierney <59835292+caroleatierney@users.noreply.github.com>
2023-02-13 18:32:24 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require['express');
+
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send("Hellow World from Back4App Containers!");
+});
+
+app.listen(3333, () =>{
+  console.log('Listening at https://localhost:3333");
+});
